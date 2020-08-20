85eac4f695e833e5d069c04fc51ed375985fcb33
Steven Quach <stevendquach@gmail.com>
2020-08-20 07:02:38 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -23,5 +23,7 @@ function accessElementInArray(array, index) {
 function destructivelyRemoveElementFromBeginningOfArray(array) {
   array.shift(0)
   return array
-
+}
+function removeElementFromBeginningOfArray(array) {
+    array.slice()
 }
